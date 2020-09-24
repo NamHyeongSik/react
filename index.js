c895d5fbf3aaa17d776964dd201b7ccc0f87de72
NamHyeongSik <skagudtlr731@gmail.com>
2020-09-24 14:39:51 +0900

    로그인 구현

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const {User} = require('./models/user');
 const config = require('./config/key');
+const user = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -19,12 +21,39 @@ app.get('/', (req,res)=>{res.send('EXPRESS')});
 
 app.post('/register', (req,res)=>{
     const user = new User(req.body)
+    //.save는 mongodb의 method 
     user.save((err, doc)=>{
         if(err) return res.json({success: false, err});
         return res.status(200).json({
             success: true
         });
     });
+})
+
+app.post('login', (req,res)=>{
+    User.findOne({email: req.body.email}, (err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                messgae: "없는 이메일입니다."
+            });
+        }
+        //custom method, user.js에서 만든다
+        user.comparePassword(req.body.password, (err,isMatch)=>{
+            if(!isMatch) 
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+            
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                //토큰을 저장 : 쿠키 or localstorage or session, 여기서는 cookie에 저장
+                res.cookie("_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id});
+            });
+        });
+    });
+
+    
 })
 
 app.listen(port,()=>{console.log(`Example app listening on port ${port}`)});
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;      //salt를 이용해서 비밀번호 암호화, 그 salt의 자릿수
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -26,6 +29,39 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+//pre도 mongoose에서 가져온 method, user에 data저장하기 전에 실행된다.
+userSchema.pre('save', function(next){
+    let user = this;    //index.js에서 req.body로 받아오므로 this가 userSchema를 가리킬 수 있음.
+    if(user.isModified('password')){    //password가 수정될때만 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+    bcrypt.compare(plainPassword, this.password, (err,isMatch)=>{
+        if(err) return callback(err);
+        callback(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function(callback){
+    let user = this;
+
+    let token = jwt.sign(user._id.toHexString(), 'secretToken');
+    
+    user.token = token;
+    user.save((err,user)=>{
+        if(err) return callback(err);
+        callback(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User};
